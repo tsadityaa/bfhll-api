4aeeea295593fa85ca4ac63f88688a1ed1ae2653
tsadityaa <124493757+tsadityaa@users.noreply.github.com>
2025-08-29 12:02:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require("./index");
 
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("API is alive , do the post request https://bfhll-api.onrender.com/bfhl ");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
